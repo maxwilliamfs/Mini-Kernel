483e9120e44bc88a54edb50503298e08db7e6b2b
MegaMex <williammaxbcc4794@gmail.com>
2026-02-10 18:01:50 -0300

    FileSystem UPDATE

--- src/FileSystem.c
+++ src/FileSystem.c
@@ -24,7 +24,9 @@ void editFile(char nome[64]){
     fclose(arq);
     while(1){
         clear();
-        printf("------------------- MODO EDICAO ------------------------\n\n\n");
+        printf("------------------------------------- MODO EDICAO ----------------------------------------------\n\n");
+        printf("------- Conteudo do Arquivo -------\n\n");
+        readFile(nome);
         printf("Informe um comando de edicao...\n");
         char *comando, *argumento, entrada[100];
         fgets(entrada,100,stdin);
@@ -38,7 +40,10 @@ void editFile(char nome[64]){
         } else if(strcmp(comando,"exit") == 0){
             
         } else if(strcmp(comando,"help") == 0){
-            
+            printf("\n<help> Informa os comandos validos");
+            printf("<write x> Escreve a linha x no arquivo");
+            printf("<delete x> Apaga x linhas do arquivo");
+            printf("<exit> Sai do modo edicao\n\n");
         } else {
             printf("Comando invalido!!!\n");
         }
@@ -48,7 +53,13 @@ void writeFile(char nome[64]){
 
 }
 void readFile(char nome[64]){
-    
+    FILE *arq = fopen(nome,"r");
+    char leitura[100];
+    while (fgets(leitura,100,arq) != NULL) {
+        printf("%s", leitura);
+    }   
+    printf("\n");
+    printf("-----------------------------------\n\n");
 }
 void clear(){
     system("clear");
